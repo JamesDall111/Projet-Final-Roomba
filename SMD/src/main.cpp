903e75cd4ae4d980499328cda02c02d7f49cddee
JamesDall111 <jamesdallaire111@gmail.com>
2026-04-13 09:51:45 -0400

    Add SMD PlatformIO project scaffold
    Initialize SMD project with PlatformIO and VS Code workspace files.

---
+++ SMD/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+
+void setup() 
+{
+  Serial.begin(115200);
+  Serial.println("Hello Roomba 🔥");
+}
+
+void loop() 
+{
+  Serial.println("Running...");
+  delay(1000);
+    Serial.println("caca...");
+
+}
